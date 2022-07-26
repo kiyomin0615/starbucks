dbe942a4eecf1891dfbd1116b91d2fc0f9e2072c
kiyomin0615 <moeday1010@gmail.com>
2022-07-26 13:26:48 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,22 @@
+// document 객체는 html 요소
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2022
